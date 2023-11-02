409baa66599d80b04437a8b481561de88b82e317
cgdahvis <30879821+cgdahvis@users.noreply.github.com>
2023-11-01 22:23:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-import re
+import pandas as pd
+
+# Create an empty DataFrame to store the data for FX Derivative Order Tracker
+data = pd.DataFrame(columns=['Client Name', 'CCY Pair', 'Structure', 'Liquidity Provider', 'Level', 'Client Fill Level'])
 
 # Set title
 st.set_page_config(page_title="Trade Idea Generator", page_icon=":chart_with_upwards_trend:", layout="wide")
@@ -29,7 +32,6 @@ for i in range(num_strikes):
     label = f'ERKO' if (option_type in ['call ERKO', 'put ERKO'] and i == 1) else f'RKI' if (option_type in ['call spread RKI', 'put spread RKI'] and i == 2) else f'Strike {i+1}'
     with eval(f'col_strike{i+1}'):
         strikes.append(st.number_input(label, format='%f'))
-
 
 # Conditional input for Cost with unit based on option type
 if option_type == 'digital':
@@ -72,25 +74,13 @@ if st.button('Generate Trade Idea'):
     """
     st.markdown(trade_idea_html, unsafe_allow_html=True)
 
-#for adding the tracking live order functionality
-# Create an empty DataFrame to store the data for FX Derivative Order Tracker
-data = pd.DataFrame(columns=['Client Name', 'CCY Pair', 'Structure', 'Liquidity Provider', 'Level', 'Client Fill Level'])
-
-# Set title
-st.set_page_config(page_title="Trade Idea Generator", page_icon=":chart_with_upwards_trend:", layout="wide")
-st.title('Trade Idea Generator')
-
-# Create a navigation menu
+# Display the FX Derivative Order Tracker in a separate tab
 menu_option = st.sidebar.radio("Navigation Menu", ["Trade Idea Generator", "FX Derivative Order Tracker"])
 
 if menu_option == "Trade Idea Generator":
-    # Your existing code for the Trade Idea Generator
     pass
 elif menu_option == "FX Derivative Order Tracker":
     st.header("FX Derivative Order Tracker")
-
-    # Create an empty DataFrame to store the data
-    data = pd.DataFrame(columns=['Client Name', 'CCY Pair', 'Structure', 'Liquidity Provider', 'Level', 'Client Fill Level'])
 
     # Sidebar for data entry
     st.sidebar.header("Add Order")
